356fb6f6783e4fb916bd9eceec3be05bcac44518
kennyvb <kenny@kennyvb.dk>
2012-12-16 22:29:18 +0100

    2nd init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OctoDangerzone::Application.config.secret_token = 'e118563e7fea32460b37f12f21ebdeb6b1e473ad919c50f28f3ca0b64a13c3520138f58077484efacdadc3826c5b73cab268e1056529c99bca241a7a5bfafd75'
